45fe2a63cdecbd467cb3443e709c5a326a35da99
dikkar007 <dikkar007@gmail.com>
2017-06-16 10:34:01 -0400

    T3
    T3

---
+++ scripts/control.js
@@ -0,0 +1,87 @@
+/*
+ * object to contain all items accessable to all control functions
+ */
+var globals = {};
+var user_choice="X";
+var ai_option="O";
+/*
+ * choosing difficulty level (onclick span.level) behavior and control
+ * when a level is clicked, it becomes highlighted and the "ai.level" variable
+ * is set to the chosen level
+ */
+$(".level").each(function() {
+    var $this = $(this);
+    $this.click(function() {
+        $('.selected').toggleClass('not-selected');
+        $('.selected').toggleClass('selected');
+        $this.toggleClass('not-selected');
+        $this.toggleClass('selected');
+
+        ai.level = $this.attr("id");
+    });
+});
+
+$(".user_choice").each(function() {
+    var $this = $(this);
+    $this.click(function() {
+        $('.selectedu').toggleClass('not-selectedu');
+        $('.selectedu').toggleClass('selectedu');
+        $this.toggleClass('not-selectedu');
+        $this.toggleClass('selectedu');
+
+        if( $this.attr("id") == "O"){
+             user_choice="O";
+             ai_option="X";
+           }
+    });
+});
+
+/*
+ * start game (onclick div.start) behavior and control
+ * when start is clicked and a level is chosen, the game status changes to "running"
+ * and UI view to swicthed to indicate that it's human's trun to play
+ */
+$(".start").click(function() {
+    var selectedDiffeculty = $('.selected').attr("id");
+    if(typeof selectedDiffeculty !== "undefined") {
+        var aiPlayer = new AI(selectedDiffeculty);
+        globals.game = new Game(aiPlayer);
+
+        aiPlayer.plays(globals.game);
+
+        globals.game.start();
+
+        $('.intial',this).hide();
+        $("#board.board").show();
+
+    }
+});
+
+$(".ingame").click(function() {
+    location.reload();
+});
+
+/*
+ * click on cell (onclick div.cell) behavior and control
+ * if an empty cell is clicked when the game is running and its the human player's trun
+ * get the indecies of the clickd cell, craete the next game state, upadet the UI, and
+ * advance the game to the new created state
+ */
+ $(".cell").each(function() {
+     var $this = $(this);
+     $this.click(function() {
+         if(globals.game.status === "running" && globals.game.currentState.turn === "X" && !$this.hasClass('occupied')) {
+             var indx = parseInt($this.data("indx"));
+
+             var next = new State(globals.game.currentState);
+             next.board[indx] = "X";
+
+             ui.insertAt(indx, user_choice);
+
+             next.advanceTurn();
+
+             globals.game.advanceTo(next);
+
+         }
+     })
+ });
